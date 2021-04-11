d3470c25c45518ab7af0e10ed2b52abdb1e62d85
OderoOluoch <oderoluoch@gmail.com>
2021-04-11 20:50:26 +0300

    Put validation for the data being input

--- JS/index.js
+++ JS/index.js
@@ -2,10 +2,22 @@ let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday
 let maleKhanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleKhanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+let formValidation = function(){
+    let dateData = document.forms["khanEntry"]["data"].value
+    let genderData = document.forms["khanEntry"]["gender"].value
+
+    if(dateData == "" || genderData == ""){
+      alert("Ensure all inputs are entered correctly to get your Khan name")   
+    }else{
+        getUserDetails()
+    }
+}
+
 let getUserDetails = function(){
-    data = document.getElementById("data").value;
-    gender = document.getElementById("gender").value
+    let data = document.getElementById("data").value;
+    let gender = document.getElementById("gender").value
     let genDate = new Date(data).getDay();
+    
     getAkhanName(genDate,gender)
 }
 
@@ -13,10 +25,10 @@ let display = document.getElementById("display")
 
 let getAkhanName = function(day, gender){
     if(gender === "Male"){
-        display.innerHTML = `<p>Your Akan name is ${maleKhanNames[day]} becuase you were born on ${daysArray[day]}</p>`
+        display.innerHTML = `<p>Your Akan name is ${maleKhanNames[day]} becuase you were a ${gender} born on ${daysArray[day]}</p>`
 
     }else if(gender === "Female"){
-        display.innerHTML = `<p>Your Akan name is ${femaleKhanNames[day]} becuase you were born on ${daysArray[day]}</p>`
+        display.innerHTML = `<p>Your Akan name is ${femaleKhanNames[day]} becuase you are a ${gender} born on ${daysArray[day]}</p>`
     }
 }
 
