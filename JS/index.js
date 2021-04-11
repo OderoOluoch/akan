c2b03c4af244e2b0f63d190f0a0c257fa792f484
OderoOluoch <oderoluoch@gmail.com>
2021-04-11 15:25:58 +0300

    managed to get data from the user input

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,6 @@
-alert("meeeee")
+let getUserDetails = function(){
+    data = document.getElementById("data").value;
+    console.log(data)
+    let genDate = new Date(data).getDay();
+    console.log(genDate)
+}
