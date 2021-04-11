f94db77ea61d367f647507b3c7e3e96889bf598c
OderoOluoch <oderoluoch@gmail.com>
2021-04-11 18:48:46 +0300

    Logic for the name and gender

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,20 @@
-
+let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 
 let getUserDetails = function(){
     data = document.getElementById("data").value;
+    gender = document.getElementById("gender").value
     console.log(data)
     let genDate = new Date(data).getDay();
     console.log(genDate)
+}
+
+let getAkhanName = function(day, gender){
+    if(gender === "Male"){
+        console.log(day, gender)
+
+    }else if(gender === "Female"){
+        console.log(day,gender)
+    }
+
 }
